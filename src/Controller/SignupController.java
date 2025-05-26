8105b71da647e92a33966c2699e326e91a88f69c
Anuskaneupane69 <69210anuskarbs@gmail.com>
2025-05-26 12:59:01 +0545

    initial project

---
+++ src/Controller/SignupController.java
@@ -0,0 +1,52 @@
+package Controller;
+
+import Dao.UserDao;
+import model.User;
+import view.Signup;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+
+public class SignupController {
+    private final UserDao userDao = new UserDao();
+    private final Signup userView;
+
+    public SignupController(Signup userView) {
+        this.userView = userView;
+        userView.addAddUserListener(new AddUserListener());
+    }
+
+    public void open() {
+        this.userView.setVisible(true);
+    }
+
+    public void close() {
+        this.userView.dispose();
+    }
+
+    class AddUserListener implements ActionListener {
+
+        AddUserListener() {
+        }
+        public void actionPerformed(ActionEvent e) {
+            try {
+                String name = userView.getUsernameField().getText();
+                String email = userView.getEmailField().getText();
+                String password = new String(userView.getPasswordField().getPassword());
+                
+                User user = new User(name, email, password);
+                
+                boolean check = userDao.CheckUser(user);
+                if (check) {
+                    JOptionPane.showMessageDialog(userView, "Duplicate user");
+                } else {
+                    userDao.signup(user);
+                    JOptionPane.showMessageDialog(userView, "User registered successfully");
+                }
+            } catch (Exception ex) {
+                System.out.println("Error adding user: " + ex.getMessage());
+                ex.printStackTrace();
+            }
+        }
+    }
+}
