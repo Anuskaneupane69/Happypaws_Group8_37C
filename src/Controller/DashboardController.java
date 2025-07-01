9b1b092674f7d40db5c018f20f26b675ff74e9f7
Anuskaneupane69 <69210anuskarbs@gmail.com>
2025-07-01 13:26:13 +0545

    restored the whole file

--- src/Controller/DashboardController.java
+++ src/Controller/DashboardController.java
@@ -14,3 +14,22 @@ import view.ProductFrame;
 
 
 
+private void initListeners() {
+        updateButton.addActionListener((ActionEvent e) -> {
+            try {
+                double newWeight = Double.parseDouble(weightField.getText());
+                currentWeight = newWeight;
+                // In real case, update to database here
+                lastUpdatedLabel.setText("Last Updated Weight : " + currentWeight + " kgs (Today)");
+                JOptionPane.showMessageDialog(null, "Weight updated successfully!");
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "Invalid weight. Please enter a number.");
+            }
+        });
+
+        certButton.addActionListener(e -> JOptionPane.showMessageDialog(null, "Open Certification Page"));
+        feedButton.addActionListener(e -> JOptionPane.showMessageDialog(null, "Open Feeding Tracker"));
+        galleryButton.addActionListener(e -> JOptionPane.showMessageDialog(null, "Open Gallery"));
+        appointmentButton.addActionListener(e -> JOptionPane.showMessageDialog(null, "Book Appointment Page"));
+    }
+   
